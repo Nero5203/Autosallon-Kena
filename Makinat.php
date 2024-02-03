516ce032a174286f61f890d3622f171b0bf0c750
Luka.M <149681931+Nero5203@users.noreply.github.com>
2024-02-03 22:18:19 +0100

    Add files via upload

--- Makinat.php
+++ Makinat.php
@@ -24,19 +24,19 @@
                 <div class="logo">
                     <a href="/index.html"><img src="img/car-logo.png" id="myFunction" alt=""></a>
                     <ul>
-                        <a href="evente.html" id="myFunction">
+                        <a href="evente.php" id="myFunction">
                             <li style="color: black;">Veturat Per Evente</li>
                         </a>
-                        <a href="index.html">
+                        <a href="index.php">
                             <li style="color: black;">Autosalloni</li>
                         </a>
-                        <a href="contact.html" id="myContact">
+                        <a href="contact.php" id="myContact">
                             <li style="color: black;">Na Kontaktoni</li>
                         </a>
                         <a href="sign-in.html" id="sign">
                             <li style="color: black;">Sign-in</li>
                         </a>
-                        <a href="Makinat.html" id="veturat">
+                        <a href="Makinat.php" id="veturat">
                             <li style="color: black;">makinat</li>
                         </a>
 
@@ -55,28 +55,28 @@
                 <form action="vetura.php" method="post">
                     <div class="first">
                         <img src="img/car1.jpg" width="300px" height="250px" alt="">
-                        <h3>Cadillac Eldorado</h3>
-                        <p>Cadillac Eldorado, Coral Coupe, <br> Viti 1972, Motori 4’200 cm2, PS 270</p>
-                        <h4>Cmimi fillon nga 275 €</h4>
+                        <h3>Audi</h3>
+                        <p>Audi a4, ngjyr e bardh, <br> Viti 2016, Motori 4’200 cm2, PS 270</p>
+                        <h4>Cmimi 22000€</h4>
                         <button class="btn">Shiko me shume</button>
                 </form>
             </div>
             <form action="vetura2.php" method="post">
                 <div class="second">
                     <img src="img/car2.jpg" width="300px" height="250px" alt="">
-                    <h3>Cadillac Eldorado</h3>
-                    <p>EXCALIBUR Style, Lincoln M, Motori <br> 4’600 mc2, PS 320 <br></p>
-                    <h4>Cmimi fillon nga 340 €</h4>
+                    <h3>Audi</h3>
+                    <p>Audi Rs7, 2022, Motori <br> 4’600 mc2, PS 320 <br></p>
+                    <h4>Cmimi 90000€</h4>
                     <button class="btn">Shiko me shume</button>
             </form>
         </div>
         <form action="vetura3.php" method="post">
             <div class="third">
                 <img src="img/car3.jpg" width="300px" height="250px" alt="">
-                <h3>EXCALIBUR Style, Lincoln L</h3>
-                <p>EXCALIBUR Style, Lincoln L Motori <br>
+                <h3>Porsche</h3>
+                <p>Porsche panamera, six boxer engines <br>
                     4’600 mc2, PS 320 <br> </p>
-                <h4>Cmimi fillon nga 390 €</h4>
+                <h4>Cmimi 146000€</h4>
                 <button class="btn">Shiko me shume</button>
             </div>
         </form>
@@ -89,28 +89,28 @@
         <form action="vetura4.php" method="post">
             <div class="first">
                 <img src="img/car4.jpg" width="300px" height="250px" alt="">
-                <h3>Cadillac Eldorado</h3>
-                <p>Cadillac Eldorado, Coral Coupe, <br> Viti 1972, Motori 4’200 cm2, PS 270</p>
-                <h4>Cmimi fillon nga 275 €</h4>
+                <h3>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;BMW</h3>
+                <p>BMW X5,<br> Viti 2018, Motori 4’100 cm2, PS 220</p>
+                <h4>Cmimi 64000€</h4>
                 <button class="btn">Shiko me shume</button>
             </div>
         </form>
         <form action="vetura5.php" method="post">
             <div class="second">
                 <img src="img/car5.jpg" width="300px" height="250px" alt="">
-                <h3>Cadillac Eldorado</h3>
-                <p>EXCALIBUR Style, Lincoln M, Motori <br> 4’600 mc2, PS 320 <br></p>
-                <h4>Cmimi fillon nga 340 €</h4>
+                <h3>Cadillac</h3>
+                <p>EXCALIBUR Style, Lincoln M, Motori <br> 4’200 mc2, PS 150 <br></p>
+                <h4>Cmimi 21000€</h4>
                 <button class="btn">Shiko me shume</button>
             </div>
         </form>
         <form action="vetura6.php" method="post">
             <div class="third">
                 <img src="img/car6.jpg" width="300px" height="250px" alt="">
-                <h3>EXCALIBUR Style, Lincoln L</h3>
-                <p>EXCALIBUR Style, Lincoln L Motori <br>
-                    4’600 mc2, PS 320 <br> </p>
-                <h4>Cmimi fillon nga 390 €</h4>
+                <h3>Mustang</h3>
+                <p>Quatro duo Motori <br>
+                    mc4, PS 450 <br> </p>
+                <h4>Cmimi 77000€</h4>
                 <button class="btn">Shiko me shume</button>
             </div>
         </form>
@@ -124,28 +124,28 @@
         <form action="vetura7.php" method="post">
             <div class="first">
                 <img src="img/car7.jpg" width="300px" height="250px" alt="">
-                <h3>Cadillac Eldorado</h3>
-                <p>Cadillac Eldorado, Coral Coupe, <br> Viti 1972, Motori 4’200 cm2, PS 270</p>
-                <h4>Cmimi fillon nga 275 €</h4>
+                <h3>Peugeot</h3>
+                <p>Station wagon version of the 308., <br> Viti 2014, Motori 2’500 cm1, PS 160</p>
+                <h4>Cmimi 14000€</h4>
                 <button class="btn">Shiko me shume</button>
             </div>
         </form>
         <form action="vetura8.php" method="post">
             <div class="second">
                 <img src="img/car9.jpg" width="300px" height="250px" alt="">
-                <h3>Cadillac Eldorado</h3>
-                <p>EXCALIBUR Style, Lincoln M, Motori <br> 4’600 mc2, PS 320 <br></p>
-                <h4>Cmimi fillon nga 340 €</h4>
+                <h3>Lamborghini</h3>
+                <p>Lamborghini Urus, Motori <br> 5’600 mc2, PS 520 <br></p>
+                <h4>Cmimi 230000€</h4>
                 <button class="btn">Shiko me shume</button>
             </div>
         </form>
         <form action="vetura9.php" method="post">
             <div class="third">
                 <img src="img/carnew.jpg" width="300px" height="250px" alt="">
-                <h3>EXCALIBUR Style, Lincoln L</h3>
-                <p>EXCALIBUR Style, Lincoln L Motori <br>
-                    4’600 mc2, PS 320 <br> </p>
-                <h4>Cmimi fillon nga 390 €</h4>
+                <h3>Ferrari</h3>
+                <p>Ferrari Enzo, L Motori <br>
+                    4’600 mc2, PS 380 <br> </p>
+                <h4>Cmimi 180000€</h4>
                 <button class="btn">Shiko me shume</button>
             </div>
         </form>
@@ -159,28 +159,28 @@
         <form action="vetura10.php" method="post">
             <div class="first">
                 <img src="img/carnew2.jpg" width="300px" height="250px" alt="">
-                <h3>Cadillac Eldorado</h3>
-                <p>Cadillac Eldorado, Coral Coupe, <br> Viti 1972, Motori 4’200 cm2, PS 270</p>
-                <h4>Cmimi fillon nga 275 €</h4>
+                <h3>Mercedes</h3>
+                <p>Mercedes Amg cla 45<br> Viti 2023, Motori 4’400 cm2, PS 280</p>
+                <h4>Cmimi 75000€</h4>
                 <button class="btn">Shiko me shume</button>
             </div>
         </form>
         <form action="vetura11.php" method="post">
             <div class="second">
                 <img src="img/carnew3.jpg" width="300px" height="250px" alt="">
-                <h3>Cadillac Eldorado</h3>
-                <p>EXCALIBUR Style, Lincoln M, Motori <br> 4’600 mc2, PS 320 <br></p>
-                <h4>Cmimi fillon nga 340 €</h4>
+                <h3>Lamborghini</h3>
+                <p>Lamborghini Aventador, M, Motori <br> 4’600 mc2, PS 720 <br></p>
+                <h4>Cmimi 330000€</h4>
                 <button class="btn">Shiko me shume</button>
             </div>
         </form>
         <form action="vetura12.php" method="post">
             <div class="third">
                 <img src="img/carnew4.jpg" width="300px" height="250px" alt="">
-                <h3>EXCALIBUR Style, Lincoln L</h3>
-                <p>EXCALIBUR Style, Lincoln L Motori <br>
-                    4’600 mc2, PS 320 <br> </p>
-                <h4>Cmimi fillon nga 390 €</h4>
+                <h3>Jeep</h3>
+                <p>Jeep A, 2.0 Motori <br>
+                    , PS 320 <br> </p>
+                <h4>Cmimi 19000€</h4>
                 <button class="btn">Shiko me shume</button>
             </div>
         </form>
