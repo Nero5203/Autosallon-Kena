205bed493e4df8a46c1ed0de58fedf2c504d3405
Luka.M <149681931+Nero5203@users.noreply.github.com>
2024-02-03 22:17:01 +0100

    Add files via upload

---
+++ evente.php
@@ -0,0 +1,187 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,200&display=swap" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@1,200;1,300&display=swap" rel="stylesheet">
+    <title>Auto Kena SHPK</title>
+</head>
+
+<body>
+    <header>
+        <section>
+            <div class="all">
+                <div class="logo">
+                    <a href="/index.php"><img src="img/car-logo.png" id="myFunction" alt=""></a>
+                    <ul>
+                        <a href="evente.php" id="myFunction">
+                            <li>Veturat Per Evente</li>
+                        </a>
+                        <a href="index.php">
+                            <li>Autosalloni</li>
+                        </a>
+                        <a href="contact.php" id="myContact">
+                            <li>Na Kontaktoni</li>
+                        </a>
+                        <a href="sign-in.html" id="sign">
+                            <li>Sign-in</li>
+                        </a>
+                        <a href="haveAcc.html" id="veturat">
+                            <li>makinat</li>
+                        </a>
+                    </ul>
+
+                </div>
+            </div>
+    </header>
+    <div class="IMG_back1">
+        <img src="img/2222.jpg" alt="">
+    </div>
+
+
+
+
+    <div class="reserve">
+        <div class="hh">
+            <h1><em>Rezervoni veturën</em> për eventin tuaj!</h1>
+        </div>
+        <div>
+            <!-- <h3>Dëshironi të ndjeheni mbret i eventit tuaj të rëndësishëm, <br> atëherë ja ku e keni mundësinë.</h3> -->
+        </div>
+        <div>
+            <button><a href="#first">shiko makinat</a></button>
+        </div>
+    </div>
+
+    <br><br>
+    </section>
+    <div class="events">
+        <h1></h1>
+        <h3></h3>
+
+        <div class="foto">
+
+            <div id="first">
+                <form action="carEvent1.php" method="post">
+                    <img src="img/eldorado-600x450.jpeg" width="300px" height="250px" alt="">
+                    <h3>Cadillac Eldorado</h3>
+                    <p>Cadillac Eldorado, Coral Coupe, <br> Viti 1972, Motori 4’200 cm2, PS 270</p>
+                    <h4>Cmimi fillon nga 275 €</h4>
+                    <button class="btn">Shiko me shume</button>
+                </form>
+            </div>
+            <div class="second">
+                <form action="carEvent2.php" method="post">
+                    <img src="img/lincoln-m-600x450.jpeg" width="300px" height="250px" alt="">
+                    <h3>Cadillac Eldorado</h3>
+                    <p>EXCALIBUR Style, Lincoln M, Motori <br> 4’600 mc2, PS 320 <br></p>
+                    <h4>Cmimi fillon nga 340 €</h4>
+                    <button class="btn">Shiko me shume</button>
+                </form>
+            </div>
+
+            <div class="third">
+                <form action="carEvent3.php" method="post">
+                    <img src="img/4-600x450.jpeg" width="300px" height="250px" alt="">
+                    <h3>EXCALIBUR Style, Lincoln L</h3>
+                    <p>EXCALIBUR Style, Lincoln L Motori <br>
+                        4’600 mc2, PS 320 <br> </p>
+                    <h4>Cmimi fillon nga 390 €</h4>
+                    <button class="btn">Shiko me shume</button>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="events">
+
+        <div class="foto">
+            <div class="first">
+                <form action="carEvent4.php" method="post">
+                    <img src="img/WhatsApp-Image-2020-12-22-at-19.50.51-600x450.jpeg" width="300px" height="250px" alt="">
+                    <h3>Cadillac Eldorado</h3>
+                    <p>Cadillac Eldorado, Coral Coupe, <br> Viti 1972, Motori 4’200 cm2, PS 270</p>
+                    <h4>Cmimi fillon nga 275 €</h4>
+                    <button class="btn">Shiko me shume</button>
+                </form>
+            </div>
+            <div class="second">
+                <form action="carEvent5.php" method="post">
+                    <img src="img/lincoln-600x400.jpeg" width="300px" height="250px" alt="">
+                    <h3>Cadillac Eldorado</h3>
+                    <p>EXCALIBUR Style, Lincoln M, Motori <br> 4’600 mc2, PS 320 <br></p>
+                    <h4>Cmimi fillon nga 340 €</h4>
+                    <button class="btn">Shiko me shume</button>
+                </form>
+            </div>
+            <div class="third">
+                <form action="carEvent6.php" method="post">
+                    <img src="img/4-600x450.jpeg" width="300px" height="250px" alt="">
+                    <h3>EXCALIBUR Style, Lincoln L</h3>
+                    <p>EXCALIBUR Style, Lincoln L Motori <br>
+                        4’600 mc2, PS 320 <br> </p>
+                    <h4>Cmimi fillon nga 390 €</h4>
+                    <button class="btn">Shiko me shume</button>
+                </form>
+            </div>
+        </div>
+    </div>
+
+    <div class="fullscreen">
+        <!-- <img src="/img/about-fullscreen-image-1-1920x600.jpg" alt=""> -->
+        <div class="who">
+            <img src="img/222-1-570x350.jpg" alt="">
+            <div class="style">
+                <h3>Kush Jemi Ne?</h3>
+                <h1>Ne Kujdesemi Per <em>Eventin tuaj</em> </h1>
+                <p>Rezervoni nje nga makinat tona te veqanta dhe behuni mbret i eventit tuaj</p>
+
+            </div>
+        </div>
+    </div>
+
+    <!-- <div class="fullscreen">
+        <div class="who">
+            <img src="/img/222-1-570x350.jpg" alt="">
+            <div class="style">
+            <h3>Kush Jemi Ne</h3>
+            <h1>Ne Kujdesemi Per <em>Eventin tuaj</em> </h1>
+            <p>Rezervoni nje nga makinat tona te veqanta dhe behuni mbret i eventit tuaj</p>
+            <button>Shiko Me Shume</button>
+        </div>
+        </div>
+    </div> -->
+
+    <br><br><br>
+    <div class="contact">
+        <div>
+            <h1>Na Kontaktoni <em>Tani</em></h1>
+            <span>Rezervoni Makinen e juaj te endrrave</span>
+        </div>
+
+        <div class="email">
+            <form action="evente.php" method="post">
+                <input name="name" type="text" id="name" placeholder="Emri" required>
+                <input name="email" id="email" type="text" placeholder="Emaili" required>
+                <input name="subject" id="subject" type="text" placeholder="Subjekti" required>
+
+                <textarea name="text" id="" cols="30" rows="5" placeholder="Mesazhi" required></textarea>
+                <button class="btn">Submit</button>
+            </form>
+        </div>
+
+
+
+    </div>
+    <footer>
+        <p>Copyright © 2023 <em>Auto Kena</em> SHPK</p>
+
+    </footer>
+</body>
+
+</html>
